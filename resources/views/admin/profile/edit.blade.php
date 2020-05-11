ca7ca5dc96d8b28ae9c9885a3ba3021a6a740530
Mikiko6276 <mikiko6276@gmail.com>
2020-05-11 01:30:39 +0000

    課題

---
+++ resources/views/admin/profile/edit.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        
+        <title>MyNews</title>
+    </head>
+    <body>
+        <h1><font color="#ff69b4">Myニュース編集画面</h1>
+    </body>
+</html>
---
+++ routes/web.php
@@ -0,0 +1,18 @@
+<?php
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the “web” middleware group. Now create something great!
+|
+*/
+
+//18-4の課題
+Route::group(['prefix' => 'admin'], function () {
+    Route::get('news/create', 'Admin\NewsController@add');
+    Route::get('profile/create', 'Admin\ProfileController@add');
+    Route::get('profile/edit', 'Admin\ProfileController@edit');
+});
