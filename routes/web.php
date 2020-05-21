b2698268ff9610fdc69303efc7fcfff39101e040
Mikiko6276 <mikiko6276@gmail.com>
2020-05-21 05:03:23 +0000

    課題 12

--- routes/web.php
+++ routes/web.php
@@ -9,14 +9,10 @@
 | contains the “web” middleware group. Now create something great!
 |
 */
-
-//18-4の課題
-Route::group(['prefix' => 'admin'], function () {
-    Route::get('news/create', 'Admin\NewsController@add')->middleware('auth');
-    Route::get('profile/create', 'Admin\ProfileController@add')->middleware('auth');
-    Route::get('profile/edit', 'Admin\ProfileController@edit')->middleware('auth');
-});
-
 Auth::routes();
-
 Route::get('/home', 'HomeController@index')->name('home');
+
+Route::group(['prefix' => 'admin', 'middleware' => 'auth'], function () {
+    Route::get('news/create', 'Admin\NewsController@add');
+    Route::post('news/create', 'Admin\NewsController@create');
+});
