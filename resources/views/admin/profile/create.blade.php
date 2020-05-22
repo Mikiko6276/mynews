ba2281dee69ec82a51295994eb8cbef8ccc492c7
Mikiko6276 <mikiko6276@gmail.com>
2020-05-22 11:29:17 +0000

    課題 13

--- resources/views/admin/profile/create.blade.php
+++ resources/views/admin/profile/create.blade.php
@@ -7,6 +7,33 @@
         <div class="row">
             <div class="col-md-8 mx-auto">
                 <h2>Myプロフィール</h2>
+                <form action="{{ action('Admin\ProfileController@create') }}" method="post" enctype="multipart/form-data">
+                    
+                    <div class="form-group row">
+                        <label class="col-md-2">name</label>
+                        <div class="col-md-10">
+                            <input type="text" class="form-control" name="title" value="{{ old('title') }}">
+                        </div>
+                    </div>
+                    <div class="form-group row">
+                        <label class="col-md-2">gender</label>
+                        <div class="col-md-2">
+                            <input type="text" class="form-control" name="title" value="{{ old('title') }}">
+                        </div>
+                    </div>
+                    <div class="form-group row">
+                        <label class="col-md-2">hobby</label>
+                        <div class="col-md-10">
+                            <input type="text" class="form-control" name="title" value="{{ old('title') }}">
+                        </div>
+                    </div>
+                    <div class="form-group row">
+                        <label class="col-md-2">introduction</label>
+                        <div class="col-md-10">
+                            <textarea class="form-control" name="bocy" rows="5">{{ old('body') }}</textarea>
+                        </div>
+                    </div>
+                </form>
             </div>
         </div>
     </div>
