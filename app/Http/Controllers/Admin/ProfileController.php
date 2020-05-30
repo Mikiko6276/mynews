b2a46b5ffdf1c0206c0ed982bc709e1f899c2982
Mikiko6276 <mikiko6276@gmail.com>
2020-05-30 07:30:13 +0000

    課題 16

--- app/Http/Controllers/Admin/ProfileController.php
+++ app/Http/Controllers/Admin/ProfileController.php
@@ -28,14 +28,51 @@ class ProfileController extends Controller
 
         return redirect('admin/profile/create');
     }
-
-    public function edit()
+    
+    public function index(Request $request)
     {
-        return view('admin.profile.edit');
+        $cond_title = $request->cond_title;
+        if ($cond_title != '') {
+            $posts = Profile::where('title', $cond_title)->get();
+        } else {
+            $posts = Profile::all();
+        }
+        return view('admin.profile.index', ['posts' => $posts, 'cond_title' => $cond_title]);
     }
 
-    public function update()
+    public function edit(Request $request)
     {
-        return redirect('admin/profile/edit');
+        $profile = Profile::find($request->id);
+        if (empty($profile)) {
+            abort(404);
+        }
+        return view('admin.profile.edit', ['profile_form' => $profile]);
+    }
+    
+    public function update(Request $request)
+    {
+        $this->validate($request, Profile::$rules);
+        $profile = Profile::find($request->id);
+        $profile_form = $request->all();
+        if (isset($profile_form['image'])) {
+            $path = $request->file('image')->store('public/image');
+            $profile->image_path = basename($path);
+            unset($profile_form['image']);
+        } elseif (isset($request->remove)) {
+            $profile->image_path = null;
+            unset($profile_form['remove']);
+        }
+        unset($profile_form['_token']);
+        
+        $profile->fill($profile_form)->save();
+        
+        return redirect('admin/profile');
+    }
+    
+    public function delete(Request $request)
+    {
+        $profile = Profile::find($request->id);
+        $profile->delete();
+        return redirect('admin/profile/');
     }
 }
