c9f442d3a216d854830cd9f3ccd01664178b66bf
Mikiko6276 <mikiko6276@gmail.com>
2020-06-11 03:24:07 +0000

    課題 17

--- app/Http/Controllers/Admin/ProfileController.php
+++ app/Http/Controllers/Admin/ProfileController.php
@@ -6,6 +6,8 @@ use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 
 use App\Profile;
+use App\ProfileHistory;
+use Carbon\Carbon;
 
 class ProfileController extends Controller
 {
@@ -62,9 +64,14 @@ class ProfileController extends Controller
             $profile->image_path = null;
             unset($profile_form['remove']);
         }
-        unset($profile_form['_token']);
         
+        unset($profile_form['_token']);
         $profile->fill($profile_form)->save();
+        
+        $profile_history = new ProfileHistory;
+        $profile_history->profile_id = $profile->id;
+        $profile_history->edited_at = Carbon::now();
+        $profile_history->save();
         
         return redirect('admin/profile');
     }
