a77dd1b03368c522093ba491d7749584084198a3
Mikiko6276 <mikiko6276@gmail.com>
2020-06-29 09:44:05 +0000

    first commit

--- routes/web.php
+++ routes/web.php
@@ -26,3 +26,7 @@ Route::group(['prefix' => 'admin'], function () {
     Route::post('profile/edit', 'Admin\ProfileController@update')->middleware('auth');
     Route::get('profile/delete', 'Admin\ProfileController@delete')->middleware('auth');
 });
+
+Route::get('/', 'NewsController@index');
+
+Route::get('/profile', 'ProfileController@index');
